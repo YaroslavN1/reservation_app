553c9a23f75ab14c5a0d50aa20d29ab18fe06c03
Yaroslav Nykyforuk <yaroslav.nykyforuk@gmail.com>
2024-01-02 17:54:41 +0100

    feat: added UnoCSS to the project.

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,15 @@ import legacy from '@vitejs/plugin-legacy';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
 import { defineConfig } from 'vite';
+import Unocss from 'unocss/vite';
+import { presetUno } from 'unocss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), legacy()],
+  plugins: [vue(), legacy(), Unocss({ presets: [presetUno()] })],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom',
   },
 });
